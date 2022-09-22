3478b08e51b7e88fd0e83146262827326b5e8835
chen-kr <kristin2@andrew.cmu.edu>
2022-09-22 15:27:44 -0400

    FIXING A HORRIBLE TYPO

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -7,7 +7,7 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.toLoserCase().contains("owo")) {
+        } else if (query.toLowerCase().contains("owo")) {
             return "what's this";
         }
         return "";
